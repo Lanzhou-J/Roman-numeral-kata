f166ef9f52826ac8541c8b6d50ce2043c908d3d2
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-29 15:52:31 +1100

    add class and function in production code, solved compiling error, test 1 fail - red bar

---
+++ RomanNumeralKata/RomanNumeralGenerator.cs
@@ -0,0 +1,10 @@
+namespace RomanNumeralKata
+{
+    public class RomanNumeralGenerator
+    {
+        public static string ToRomanNumeral(int number)
+        {
+            return null;
+        }
+    }
+}
