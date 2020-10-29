97652918cba7d2bb5e8e07f3afa844f9ac02b315
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-29 15:48:25 +1100

    add test 1 - faile to compile

---
+++ RomanNumeralKataTests/RomanNumeralTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace RomanNumeralKataTests
+{
+    public class RomanNumeralTests
+    {
+        [Fact]
+        public void ToRomanNumeralShould_ReturnI_WhenInput0()
+        {
+            var result = RomanNumeralGenerator.ToRomanNumeral(1);
+            Assert.Equal("", result);
+        }
+        
+        // [Fact]
+        // public void ToRomanNumeralShould_ReturnI_WhenInput1()
+        // {
+        //     var result = RomanNumeralGenerator.ToRomanNumeral(1);
+        //     Assert.Equal("I", result);
+        // }
+    }
+}
